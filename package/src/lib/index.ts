6d3780cc81288d2cf7da97caa3a1382074a55469
eclipher <131247135+eclipher@users.noreply.github.com>
2025-06-17 21:56:22 +0800

    refactor: migrate to monorepo

---
+++ package/src/lib/index.ts
@@ -0,0 +1,2 @@
+import "./style.css";
+export { Toaster } from "./core/Toaster";
--- src/lib/index.ts
+++
@@ -1,6 +0,0 @@
-import { Toaster } from "./Toaster";
-
-import "./style.css";
-
-export { Toaster };
-export default Toaster;
