0e09a073816c28768017888d37266a22a58f4ba6
eclipher <131247135+eclipher@users.noreply.github.com>
2025-06-17 23:38:54 +0800

    docs: migrate doc page / playground to a vue app
    vue syntax is so weird

---
+++ docs/src/component/button.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { ButtonHTMLAttributes } from "vue";
+
+const { type = "button" } = defineProps<{
+    type?: ButtonHTMLAttributes["type"];
+}>();
+</script>
+
+<template>
+    <button :type><slot /></button>
+</template>
+
+<style scoped>
+button {
+    font-family: "Inter", sans-serif;
+    padding: 10px 20px;
+    border-radius: 6px;
+    border: none;
+    cursor: pointer;
+    font-size: 14px;
+    font-weight: 500;
+    transition:
+        transform 0.2s,
+        box-shadow 0.2s;
+
+    &:hover {
+        transform: translateY(-2px);
+        box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+    }
+}
+</style>
