203194f241d922003a0d81e938097caf5fe46088
eclipher <131247135+eclipher@users.noreply.github.com>
2025-06-20 20:48:35 +0800

    docs: add vercel analytics to doc site

--- docs/src/main.ts
+++ docs/src/main.ts
@@ -1,6 +1,10 @@
 import { createApp } from "vue";
+import { inject } from "@vercel/analytics";
+
 import "./style.css";
 
 import App from "./App.vue";
+
+inject(); // Initialize Vercel Analytics
 
 createApp(App).mount("#app");
