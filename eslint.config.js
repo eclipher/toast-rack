25cea302631ac13b3a1db2ff76402e229531609f
eclipher <131247135+eclipher@users.noreply.github.com>
2025-09-22 19:08:34 +0800

    chore: add lit and related lint rules and tsconfig

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,8 @@ import { includeIgnoreFile } from "@eslint/compat";
 import globals from "globals";
 import { fileURLToPath } from "node:url";
 import ts from "typescript-eslint";
+import lit from "eslint-plugin-lit";
+import webComponents from "eslint-plugin-wc";
 
 const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url));
 
@@ -12,6 +14,9 @@ export default ts.config(
     js.configs.recommended,
     ...ts.configs.recommended,
     prettier,
+    lit.configs["flat/recommended"],
+    webComponents.configs["flat/recommended"],
+
     {
         languageOptions: {
             globals: { ...globals.browser },
